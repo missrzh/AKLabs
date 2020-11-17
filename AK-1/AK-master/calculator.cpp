3e7ed83d59117a6fcb7df08eac272fbb7e0bb2c6
missrzh <53384266+missrzh@users.noreply.github.com>
2020-11-18 00:18:00 +0200

    Add files via upload

---
+++ AK-1/AK-master/calculator.cpp
@@ -0,0 +1,16 @@
+#include "calculator.h"
+
+int Calculator::Add (int a, int b)
+{
+    return a + b;
+}
+
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5;
+}
+// Hello
+// my name Misha
+// #3
+// #4
+// bye
